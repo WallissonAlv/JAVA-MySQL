9b59f50673f1d72d56a0aa43a0f6ca21b6118d63
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-11 12:16:33 -0300

    Commit - Quebrando a cabeça aqui

--- Crud_Java_/src/ConexaoDAO.java
+++ Crud_Java_/src/ConexaoDAO.java
@@ -1,6 +1,11 @@
 
-import java.sql.Connection;
-import javax.swing.JOptionPane;
+import java.sql.Connection; /*A classe Connection representa a conexão física
+com o banco de dados e é obtida por meio do método DriverManager.*/
+import java.sql.DriverManager; /*Para abrir uma conexão com um banco de dados, 
+precisamos utilizar sempre um driver. A classe DriverManager é a responsável 
+por se comunicar com todos os drivers que você deixou disponível.*/
+import java.sql.SQLException;
+import javax.swing.JOptionPane; 
 
 // CONEXAO DE ACESSO A OBJETOS
 public class ConexaoDAO {
@@ -10,11 +15,12 @@ public class ConexaoDAO {
         
         try {   /* VERIFICACAO: TENTAR FAZER O QUE ESTA NO PRIMEIRO COLCHETE,
             FAZER O QUE ESTA NO SEGUNDO COLCHETE(CATCH) */
-            String url = "";
-            
-        } catch (Exception erro) {
-            JOptionPane.showMessageDialog(null,erro.getMessage()); // Mensagem de Erro caso o Try não funcione
+            String url = "jdbc:mysql://localhost:3306/bancoteste?user=root&password=";
+            con = DriverManager.getConnection(url);
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ConexaoDAO " + erro.getMessage()); // Mensagem de Erro caso o Try não funcione
         }
         return con;
     }
 }
+
