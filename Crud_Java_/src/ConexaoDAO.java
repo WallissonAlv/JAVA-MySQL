9522388117c4d60f21cb526687e9845de2cc3c4f
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-11 12:04:49 -0300

    .

--- Crud_Java_/src/ConexaoDAO.java
+++ Crud_Java_/src/ConexaoDAO.java
@@ -1,8 +1,20 @@
+
 import java.sql.Connection;
+import javax.swing.JOptionPane;
+
 // CONEXAO DE ACESSO A OBJETOS
 public class ConexaoDAO {
     
     public Connection ConectaBancoDados(){
         Connection con = null;
+        
+        try {   /* VERIFICACAO: TENTAR FAZER O QUE ESTA NO PRIMEIRO COLCHETE,
+            FAZER O QUE ESTA NO SEGUNDO COLCHETE(CATCH) */
+            String url = "";
+            
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null,erro.getMessage()); // Mensagem de Erro caso o Try não funcione
+        }
+        return con;
     }
 }
