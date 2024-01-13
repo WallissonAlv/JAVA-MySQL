c5d6230cc1f39ca73c137aa39a12e9198d88eec9
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-13 14:45:26 -0300

    commit Aula10

--- Aula10.sql
+++ Aula10.sql
@@ -5,3 +5,26 @@ order by nome;
 
 select ano, nome, carga from cursos
 order by ano, nome;
+
+select * from cursos
+Where ano = '2016'
+order by nome;
+
+select nome, descricao, ano from cursos
+Where ano <= 2016
+order by ano , nome;
+
+select nome, descricao, ano from cursos
+Where ano != 2016
+order by ano , nome;
+
+select nome, ano from cursos
+Where ano between 2014 and 2016
+order by nome desc, ano asc;
+
+select idcurso, nome from cursos
+Where ano in (2014, 2016, 2020)
+order by nome, ano;
+
+select nome, carga, totaulas from cursos
+where carga > 35 and totaulas < 30;
