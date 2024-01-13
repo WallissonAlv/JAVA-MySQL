eb05d96a3b236e50cad0cec1d010da663c2830c9
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-13 16:03:45 -0300

    commit Aula11

---
+++ Aula11.sql
@@ -0,0 +1,31 @@
+select * from cursos
+
+where nome like 'P%' ;
+
+select * from cursos
+where nome like '%A' ;
+
+select * from cursos
+where nome like '%A%' ;
+
+select * from cursos
+where nome not like '%A%' ;
+
+select * from cursos
+where nome like 'ph%p' ;
+
+select * from cursos
+where nome like 'ph%p' ;
+
+select * from cursos
+where nome like 'ph%p_' ;
+
+select * from gafanhotos
+where nome like '%silva%' ;
+
+select distinct nacionalidade from gafanhotos
+order by nacionalidade;
+
+select distinct carga from cursos
+order by carga;
+
