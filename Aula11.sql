4c9ceb71a2f60997d5220850f30771e5dfcf084f
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-13 16:14:25 -0300

    commit Aula11

--- Aula11.sql
+++ Aula11.sql
@@ -29,3 +29,12 @@ order by nacionalidade;
 select distinct carga from cursos
 order by carga;
 
+select count(*) from cursos where carga > 40;
+
+select max(carga) from cursos;
+
+select * from cursos where ano = '2016';
+select max(totaulas) from cursos where ano = '2016';
+select nome, min(totaulas) from cursos where ano = '2016';
+select sum(totaulas) from cursos where ano = '2016';
+select avg(totaulas) from cursos where ano = '2016';
