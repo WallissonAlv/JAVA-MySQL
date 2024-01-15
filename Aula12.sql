617173fe09c5b15a06eb74cca980c5a038dfad61
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-15 13:00:59 -0300

    Subindo Aula12

--- Aula12.sql
+++ Aula12.sql
@@ -11,6 +11,14 @@ group by carga;
 select * from cursos where totaulas = 30;
 
 select ano, count(*) from cursos
+where totaulas > 30
 group by ano
-having count(ano) >= 5
+having ano > 2013
 order by count(*) desc;
+
+select avg(carga) from cursos;
+
+select carga, count(*) from cursos
+where ano > 2015
+group by carga
+having carga > (select avg(carga) from cursos);
