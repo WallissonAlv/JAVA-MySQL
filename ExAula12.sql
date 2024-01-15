3a6336900c0b1467d430feecfcf12023ddc266d3
Wallisson Alves <wallissonlima008@gmail.com>
2024-01-15 13:16:44 -0300

    Subindo ExAula12

---
+++ ExAula12.sql
@@ -0,0 +1,18 @@
+select profissao, count(*) from gafanhotos
+group by profissao
+order by count(*) desc;
+
+select * from gafanhotos where nascimento > '2005-01-01'
+group by nascimento
+order by nascimento;
+select sexo, nascimento, count(*) from gafanhotos
+where nascimento > '2005-01-01'
+group by sexo
+order by count(*);
+
+select nacionalidade, count(*) from gafanhotos where nacionalidade != 'brasil'
+group by nacionalidade;
+select nacionalidade, count(*) from gafanhotos where nacionalidade != 'brasil'
+group by nacionalidade
+having count(*) > 3
+order by count(*);
